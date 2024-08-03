3d2a8d237bba49c489d9158cb1fe182a7a23aa50
brownxiong <brownxiong@tencent.com>
2024-08-03 21:39:37 +0800

    Actor层面的复制始终打开，放在蓝图修改值上，C++中不维护。

--- Source/ChickenRush/ChickenBall.cpp
+++ Source/ChickenRush/ChickenBall.cpp
@@ -14,29 +14,21 @@ AChickenBall::AChickenBall():
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	SetReplicates(true);
-	SetReplicateMovement(true);
 
-	GetStaticMeshComponent()->Mobility = EComponentMobility::Movable;
-	GetStaticMeshComponent()->SetSimulatePhysics(true);
-	GetStaticMeshComponent()->OnComponentHit.AddDynamic(this , &AChickenBall::OnBallHit);
-	GetStaticMeshComponent()->SetIsReplicated(true);
-	GetStaticMeshComponent()->bReplicatePhysicsToAutonomousProxy = 1;
-
-
+	// GetStaticMeshComponent()->OnComponentHit.AddDynamic(this , &AChickenBall::OnBallHit);
+	
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
 	ProjectileMovementComponent->UpdatedComponent = GetStaticMeshComponent();
 	ProjectileMovementComponent->OnProjectileBounce.AddDynamic(this, &AChickenBall::OnBallBounce);
-	ProjectileMovementComponent->SetIsReplicated(true);
-	
-	
 }
+
+
 
 // Called when the game starts or when spawned
 void AChickenBall::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -52,17 +44,19 @@ void AChickenBall::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLife
 	// DOREPLIFETIME( AChickenBall , bHolded );
 }
 
+void AChickenBall::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
 void AChickenBall::PickUpBall()
 {
-	SetReplicates(false);
-	SetReplicateMovement(false);
-	
+	// SetReplicates(false);
+	// SetReplicateMovement(false);
 	
 	GetStaticMeshComponent()->SetSimulatePhysics(false);
 	GetStaticMeshComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	GetStaticMeshComponent()->SetEnableGravity(false);
-	GetStaticMeshComponent()->SetIsReplicated(false);
-
 
 	// 禁用ProjectileMovementComponent
 	if (ProjectileMovementComponent)
@@ -74,13 +68,10 @@ void AChickenBall::PickUpBall()
 
 void AChickenBall::ThrowBall(const FVector& Direction)
 {
-	SetReplicates(true);
-	SetReplicateMovement(true);
-	
+
 	GetStaticMeshComponent()->SetSimulatePhysics(false);
 	GetStaticMeshComponent()->SetEnableGravity(false);
 	GetStaticMeshComponent()->SetCollisionEnabled( ECollisionEnabled::QueryAndPhysics );
-	GetStaticMeshComponent()->SetIsReplicated(true);
 
 	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
 	ProjectileMovementComponent->InitialSpeed = 1500.0f;
@@ -94,34 +85,39 @@ void AChickenBall::ThrowBall(const FVector& Direction)
 	FDetachmentTransformRules Rules( EDetachmentRule::KeepWorld , true );
 	DetachFromActor( Rules );
 	bHolded = false;
-
+	
+	// SetReplicates(true);
+	// SetReplicateMovement(true);
+	
 	UKismetSystemLibrary::PrintString(GetWorld(),TEXT("Ball Throw Ball ") );
 }
 
 void AChickenBall::OnBallBounce(const FHitResult& ImpactResult, const FVector& ImpactVelocity)
 {
-
-
-	UKismetSystemLibrary::PrintString(GetWorld(),TEXT("On Ball Bounce ") );
-	
-}
-
-void AChickenBall::OnBallHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
-	FVector NormalImpulse, const FHitResult& Hit)
-{
-	AChickenRushCharacter* crc = Cast<AChickenRushCharacter>(OtherActor);
+	UKismetSystemLibrary::PrintString(GetWorld(),TEXT("On Ball Bounce: ") + ImpactResult.Actor->GetName() );
+	AChickenRushCharacter* crc = Cast<AChickenRushCharacter>(ImpactResult.Actor);
 	// TODO 除了射球时的持球者外，其余物体都应该有碰撞
 	if( crc == nullptr )
 	{
 		GetStaticMeshComponent()->SetSimulatePhysics(true);
 		GetStaticMeshComponent()->SetEnableGravity(true);
-
-		ProjectileMovementComponent->Deactivate();
-		
-		UKismetSystemLibrary::PrintString(GetWorld(),TEXT("On Ball Hit: ") + OtherActor->GetName() );
+		// ProjectileMovementComponent->Deactivate();
 	}
-
 }
+
+// void AChickenBall::OnBallHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+// 	FVector NormalImpulse, const FHitResult& Hit)
+// {
+// 	UKismetSystemLibrary::PrintString(GetWorld(),TEXT("On Ball Hit: ") + OtherActor->GetName() );
+// 	AChickenRushCharacter* crc = Cast<AChickenRushCharacter>(OtherActor);
+// 	// TODO 除了射球时的持球者外，其余物体都应该有碰撞
+// 	if( crc == nullptr )
+// 	{
+// 		GetStaticMeshComponent()->SetSimulatePhysics(true);
+// 		GetStaticMeshComponent()->SetEnableGravity(true);
+// 		// ProjectileMovementComponent->Deactivate();
+// 	}
+// }
 
 
 
--- Source/ChickenRush/ChickenBall.h
+++ Source/ChickenRush/ChickenBall.h
@@ -33,6 +33,8 @@ public:
 	
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
+	virtual void PostInitializeComponents() override;
+
 	UFUNCTION()
 	void PickUpBall();
 	
